6bc38dc266289efcf1ebaca345891db94e14f10a
Abhinav Abhay <abhinavabhay1998@gmail.com>
2021-04-09 19:14:46 +0530

    new file

---
+++ window.py
@@ -0,0 +1,77 @@
+
+# Task-1 : creating a window frame 
+
+'''from tkinter import *
+
+
+
+
+
+w = Tk() # inbilt method to create a window 
+w.mainloop()'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import tkinter
+window = tkinter.Tk()
+
+
+
+
+
+'''import tkinter as tk
+window = tk.Tk()
+
+window.mainloop()'''
+
+
+
+
+
+
+
+
+# task-2: Configuring the window frame
+
+
+
+window.title("GUI")
+window.geometry('850x650')
+window.configure(background="purple2")
+
+
+
+
+
+
+window.mainloop()
+
+
